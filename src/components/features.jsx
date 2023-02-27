af189fb31b233ec226e5bdcb6b03b82691d77898
nahiliShasho <shashonahili702@gmail.com>
2023-02-27 15:07:51 -0800

    add slider to the features

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,6 +1,32 @@
 import React from "react";
-import { useEffect,useState } from "react";
-import api from "../api/page"
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import { useEffect, useState } from "react";
+import api from "../api/page";
+
+function SampleNextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style,  background: "gray"  ,border : "true",  borderRadius : "20px"}}
+      onClick={onClick}
+    />
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, background: "gray",borderRadius : "20px" }}
+      onClick={onClick}
+    />
+  );
+}
+
 export const Features = (props) => {
   const [features, setFeatures] = useState([]);
   useEffect(() => {
@@ -20,25 +46,38 @@ export const Features = (props) => {
     };
     fetchFeatures();
   });
+  var settings = {
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
   return (
-    <div id="features" className="text-center">
-      <div className="container">
-        <div className="col-md-10 col-md-offset-1 section-title">
-          <h2>Awesome Features that our customers love!</h2>
+    <div>
+      <div id="features" className="text-center">
+        <div className="container">
+          <div className="col-md-10 col-md-offset-1 section-title">
+            <h2>Awesome Features that our customers love!</h2>
+          </div>
         </div>
-        
-        <div className="row">
-          {features.map((feature) => (
-                <div key={feature.title} className="col-xs-6 col-md-3">
-                  {" "}
-                  <i className={feature.icon}></i>
-                  <h3>{feature.title}</h3>
-                  <p>{feature.text}</p>
-                </div>
-              ))
-            }
+      </div>
+      <div id="features" className="text-center">
+        <div className="container">
+          <Slider {...settings} style={{ height: "250px" }} className="row">
+            {features.map((feature) => (
+              <div key={feature.title} className="col-xs-6 col-md-3">
+                {" "}
+                <i className={feature.icon}></i>
+                <h3>{feature.title}</h3>
+                <p>{feature.text}</p>
+              </div>
+            ))}
+          </Slider>
         </div>
-        
       </div>
     </div>
   );
